a37140a19f1e11311cd773ccb9c04fc9e417246e
Javier Tintaya <jhosimart42@gmail.com>
2023-03-19 11:30:20 -0400

    Fuentes agregadas en la carpeta fonts y en el archivo tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: "'Work Sans', sans-serif",
+      cascadia: "'CascadiaCode'",
+    },
+    colors: {
+      "do-blue-dark": "rgb(8, 12, 45)",
+      "do-blue-light": "rgb(20, 86, 255)",
+      "do-blue-medium": "rgb(0, 105, 255)",
+    },
   },
   plugins: [],
 };
