6b2fcf43b906aa453036acfa86eead72e4204e48
Javier Tintaya <jhosimart42@gmail.com>
2023-03-19 21:07:07 -0400

    ningun cambio

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,16 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "do-blue-dark": "rgb(8, 12, 45)",
+        "do-blue-light": "rgb(20, 86, 255)",
+        "do-blue-medium": "rgb(0, 105, 255)",
+      },
+    },
     fontFamily: {
       sans: "'Work Sans', sans-serif",
       cascadia: "'CascadiaCode'",
-    },
-    colors: {
-      "do-blue-dark": "rgb(8, 12, 45)",
-      "do-blue-light": "rgb(20, 86, 255)",
-      "do-blue-medium": "rgb(0, 105, 255)",
     },
   },
   plugins: [],
