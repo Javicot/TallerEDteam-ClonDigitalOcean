12b4b1344fa4b1e5fe6b1540f88add3e185b72fb
Javier Tintaya <jhosimart42@gmail.com>
2023-03-21 15:43:40 -0400

    Barra de navegacion terminada, otra vez

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,17 @@ module.exports = {
     extend: {
       colors: {
         "do-blue-dark": "rgb(8, 12, 45)",
+        "do-blue-mediumDark": "rgb(29, 39, 76)",
         "do-blue-light": "rgb(20, 86, 255)",
         "do-blue-medium": "rgb(0, 105, 255)",
       },
+      fontFamily: {
+        sans: "'Work Sans', sans-serif",
+        cascadia: "'CascadiaCode'",
+      },
     },
-    fontFamily: {
-      sans: "'Work Sans', sans-serif",
-      cascadia: "'CascadiaCode'",
+    container: {
+      padding: "1.4rem",
     },
   },
   plugins: [],
