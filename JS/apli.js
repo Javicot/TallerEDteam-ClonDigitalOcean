592cd778d7653be9fcda4c89a15b0fcb9c7844b9
Javier Tintaya <jhosimart42@gmail.com>
2023-04-23 20:02:31 -0400

    Componentes en tailwind y vue creados

---
+++ JS/apli.js
@@ -0,0 +1,16 @@
+/*
+Vue.component("link-cascadia", {
+  template: `
+  <a class="text-do-blue-light font-cascadia font-semibold flex items-center" href="">Read the Docs <svg
+              xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="w-4 h-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+            </svg>
+          </a>
+  `,
+});
+
+new Vue({
+  el: "#app",
+});
+ */
